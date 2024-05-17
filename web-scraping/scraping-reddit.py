ebc1eb359d1036019a66f0e96f7b05ab1d06459c
akguerrero <akguerrero@uninorte.edu.co>
2024-05-16 23:51:14 -0500

    .

--- web-scraping/scraping-reddit.py
+++ web-scraping/scraping-reddit.py
@@ -5,7 +5,7 @@ def getdata(url):
     r = requests.get(url)
     return r.text
 
-url = 'https://www.example.com'
+url = 'https://www.reddit.com/r/learnpython/comments/78qnze/web_scraping_in_20_lines_of_code_with/'
 html_data = getdata(url)
 soup = BeautifulSoup(html_data, 'html.parser')
 print(soup)
